0950b37e0aa09aafbbc0f9223564f72795df7e05
begimaiz <zulpukarovabegimai@gmail.com>
2023-01-07 02:47:22 +0600

    settings changed

--- djangogirls/djangogirls/settings.py
+++ djangogirls/djangogirls/settings.py
@@ -117,5 +117,5 @@ STATIC_URL = "static/"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 MEDIA_URL = '/media/'
-STATIC_ROOT = Path(BASE_DIR / 'static')
+STATIC_ROOT = Path(BASE_DIR / 'djangogirls/blog/static')
 MEDIA_ROOT = Path(BASE_DIR / 'media')
